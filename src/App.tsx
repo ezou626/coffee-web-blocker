f0152515c5f0d4eba4271b03097c4a7f432fb413
Eric Zou <ezou626@gmail.com>
2024-04-28 19:25:38 -0400

    update app to have correct types

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from './components/BlockListItem';
-import { BlockList, BlockListList } from './components/BlockListList';
+import { BlockList, BlockListMetadata, Link } from './api/BlockListAPI';
+import BlockListList from './components/BlockListList';
 
 // Define a sample link for initial state when local storage is empty
 const link_1: Link = { id: 1, url: 'https://youtube.com' };
 const link_2: Link = { id: 2, url: 'https://nbc.com' };
 const link_3: Link = { id: 2, url: 'https://abc.com' };
 
-const list_1: BlockList = {
+const list_1: BlockListMetadata = {
   id: 1,
   name: "News"
 }
 
-const list_2: BlockList = {
+const list_2: BlockListMetadata = {
   id: 2,
   name: "Games"
 }
 
-const list_3: BlockList = {
+const list_3: BlockListMetadata = {
   id: 3,
   name: "Socials"
 }
@@ -29,7 +29,7 @@ const generate_selected = (set: Set<number>) => {
 
 function App() {
   // Define the state variable for storing the list of blocklists
-  const [lists, setLists] = useState<BlockList[]>([]);
+  const [lists, setLists] = useState<BlockListMetadata[]>([]);
   const [selectedLists, setSelectedLists] = useState(new Set<number>());
 
   // Use useEffect to load blocklists from local storage when the component mounts
