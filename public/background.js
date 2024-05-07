b52b81cbfa927ee38c1c70b463897256edeb6c5e
Eric Zou <ezou626@gmail.com>
2024-05-06 22:27:49 -0400

    configure initial indexeddb

--- public/background.js
+++ public/background.js
@@ -1,26 +1,44 @@
-//initialize settings
+const DB_NAME = 'BlockListDB';
+const DB_VERSION = 1; // only integers
+const BLOCKLIST_STORE = 'blockLists';
+const LINK_STORE = 'links';
+
+//initialize db, active lists, and settings
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.set({settings: {'default': true}});
   chrome.storage.local.set({active: []});
 
-  const dbRequest = indexedDB.open('BlockListDB', 1);
+  const dbRequest = indexedDB.open(DB_NAME, DB_VERSION);
 
   dbRequest.onupgradeneeded = (event) => {
+    console.log("Refreshing list");
     const db = event.target.result;
-    const blockListStore = db.createObjectStore('blockLists', { keyPath: 'id' });
-    const linksStore = db.createObjectStore('links', { keyPath: ['blockListId', 'linkId'] });
-    linksStore.createIndex('urlIndex', 'url', { unique: true });
+    const blockListStore = db.createObjectStore(BLOCKLIST_STORE, { keyPath: 'id', autoIncrement: true });
+    const linksStore = db.createObjectStore(LINK_STORE, { keyPath: 'id', autoIncrement: true });
+    linksStore.createIndex('urlIndex', 'url', { unique: false });
+    linksStore.createIndex('blockList', 'blockListId', { unique: false });
   };
 
   // initialize a default list
   dbRequest.onsuccess = (event) => {
+    //consider adding error handling here
+
     const db = event.target.result;
-    const blockListTransaction = db.transaction(['blockLists'], 'readwrite');
-    const blockListStore = blockListTransaction.objectStore('blockLists');
-    const newBlockList = { id: 1, name: 'My Block List' };
+    const blockListTransaction = db.transaction([BLOCKLIST_STORE], 'readwrite');
+    const blockListStore = blockListTransaction.objectStore(BLOCKLIST_STORE);
+    const newBlockList = { name: 'My Block List' };
     blockListStore.add(newBlockList);
-  }
-})
+    
+    const linkTransaction = db.transaction([LINK_STORE], 'readwrite');
+    const linkStore = linkTransaction.objectStore(LINK_STORE);
+    const newLink = {blockListId: 1, url: "https://youtube.com"};
+    linkStore.add(newLink);
+  };
+
+  dbRequest.onerror = (event) => {
+    console.log("IndexedDB failed! Please reload and try again!");
+  };
+});
 
 // detect open tab
 // chrome.tabs.onCreated.addListener((tab) => {
@@ -42,5 +60,5 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
       active: true,
       url: 'page.html'
     }, null);
-  }
+  };
 });
