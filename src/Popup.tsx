061ecf3168638a82d93dfc0da3a2927515cf1815
Eric Zou <ezou626@gmail.com>
2024-05-06 23:01:24 -0400

    get initial lists from db

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -3,43 +3,31 @@ import { useState, useEffect } from 'react';
 import { BlockList, BlockListMetadata, Link } from './api/BlockListAPI';
 import BlockListList from './popup_components/BlockListList';
 import BlockListUpdater from './popup_components/BlockListUpdater';
+import { DB_NAME, DB_VERSION, BLOCKLIST_STORE } from './config';
 
-const list_1: BlockListMetadata = {
-  id: 1,
-  name: "News"
-}
+// const generate_selected = (set: Set<number>) => {
+//   const array = Array.from(set); 
+//   return array.map((number) => <li key={number}>{number}</li>)
+// }
 
-const list_2: BlockListMetadata = {
-  id: 2,
-  name: "Games"
-}
-
-const list_3: BlockListMetadata = {
-  id: 3,
-  name: "Socials"
-}
-
-const generate_selected = (set: Set<number>) => {
-  const array = Array.from(set); 
-  return array.map((number) => <li key={number}>{number}</li>)
-}
-
-function Popup() {
+const Popup: React.FC = () => {
   // Define the state variable for storing the list of blocklists
   const [lists, setLists] = useState<BlockListMetadata[]>([]);
   const [selectedLists, setSelectedLists] = useState(new Set<number>());
 
   // Use useEffect to load blocklists from local storage when the component mounts
   useEffect(() => {
-    chrome.storage.local.get('lists', (result) => {
-      if (result.links === undefined) {
-        // If 'lists' key doesn't exist in local storage, set the initial state with the sample snippet
-        setLists([list_1, list_2, list_3]);
-      } else {
-        // If 'lists' key exists in local storage, set the state with the stored snippets
-        setLists(result.lists);
+    const dbRequest = indexedDB.open(DB_NAME, DB_VERSION);
+    dbRequest.onsuccess = (event) => {
+      const db: IDBDatabase = dbRequest.result;
+      const blockListTransaction = db.transaction(BLOCKLIST_STORE, 'readonly');
+      const blockListStore = blockListTransaction.objectStore(BLOCKLIST_STORE);
+      const getListsRequest = blockListStore.getAll();
+      getListsRequest.onsuccess = () => {
+        const blockLists: BlockListMetadata[] = getListsRequest.result;
+        setLists(blockLists);
       }
-    });
+    }
   }, []);
 
   const newPage = () => {
@@ -57,7 +45,7 @@ function Popup() {
         setSelectedLists={setSelectedLists}
       />
       
-      <BlockListUpdater />
+      <BlockListUpdater lists={lists} />
 
       <button onClick={newPage}>Advanced Settings</button>
     </div>
