c9cf08176fc16fb9daddfa42961342428d85b858
minfei <minfei@sas.upenn.edu>
2024-05-14 16:22:18 -0400

    Update popup color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,38 @@
 module.exports = {
   content: ['./src/**/*.{html,js,tsx,ts}'],
   theme: {
-    colors: {
-      cream: '#ece0d1',
-      tan: '#dbc1ac',
-      toast: '#967259',
-      brown: '#634832',
-      espresso: '#38220f'
-    },
+    // colors: {
+    //   cream: '#ece0d1',
+    //   tan: '#dbc1ac',
+    //   toast: '#967259',
+    //   brown: '#634832',
+    //   espresso: '#38220f'
+    // },
     extend: {
-      
+      colors: {
+        cream: '#ece0d1',
+        tan: '#dbc1ac',
+        toast: '#967259',
+        darkbrown: '#976c58',
+        espresso: '#38220f'
+      },
     },
   },
   plugins: [require("daisyui")],
 
   daisyui: {
-    themes: ["coffee", "cupcake"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#dc8850",
+          "neutral": "#3d4451",
+          "base-100": "#ece3ca",
+        },
+      },
+        "coffee", "cupcake", "retro"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "coffee", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
